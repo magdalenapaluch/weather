a011ccc71a9350f8a57d190e0bbb818f338d588c
magdalenapaluch <1magdalenapaluch@gmail.com>
2019-07-22 21:04:51 +0200

    init

---
+++ precache-manifest.577566007cf85d41d9a0c303037e8064.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0edd9231d4fa66aa5b2b837eacbcf8a3",
+    "url": "/static/media/background3.0edd9231.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "189be74aa053c5817602",
+    "url": "/static/js/main.189be74a.chunk.js"
+  },
+  {
+    "revision": "88102ac8464d9b931955",
+    "url": "/static/js/2.88102ac8.chunk.js"
+  },
+  {
+    "revision": "1077e616181426b016e65a34457e4d02",
+    "url": "/index.html"
+  }
+];
